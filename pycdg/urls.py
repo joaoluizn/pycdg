a66c1d95d838596aa231de8dca631e06ec181b37
João Luiz <joao.zenos@gmail.com>
2020-03-01 02:27:02 -0300

    Add Webpack to project

--- pycdg/urls.py
+++ pycdg/urls.py
@@ -18,10 +18,15 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.views.generic import TemplateView
 
 from teammate.routers import router
 
 urlpatterns = [
     url(r'api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('',
+         TemplateView.as_view(template_name='index.html'),
+         name='app',
+         ),
 ]
